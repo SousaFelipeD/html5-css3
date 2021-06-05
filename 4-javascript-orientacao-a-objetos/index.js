9370e552fb9c2af04eb33052a9638074d8a46793
Felipe Sousa <felipe_sousa@outlook.com>
2021-06-05 11:31:28 -0300

    Adicionando comportamentos

--- 4-javascript-orientacao-a-objetos/index.js
+++ 4-javascript-orientacao-a-objetos/index.js
@@ -1,15 +1,44 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
-    saldo;
+    _saldo;
+
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor;
+            return this._saldo;
+        }
+    }
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 8752897528;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 86379438763;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo._saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo._saldo);
+contaCorrenteRicardo.depositar(100);
+console.log(contaCorrenteRicardo._saldo);
+contaCorrenteRicardo.sacar(30);
+console.log(contaCorrenteRicardo._saldo);
+
 
 console.log(cliente1);
+console.log(cliente2);
