613ab17cdc29f7f9ef35d54fe49187e777a556fc
Felipe Sousa <felipe_sousa@outlook.com>
2021-06-05 12:34:28 -0300

    Acessando atributos privados

--- 4-javascript-orientacao-a-objetos/ContaCorrente.js
+++ 4-javascript-orientacao-a-objetos/ContaCorrente.js
@@ -1,8 +1,24 @@
+import{Cliente} from "./Cliente.js"
+
 export class ContaCorrente {
     agencia;
-    cliente;
 
+    _cliente;
     _saldo;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
 
     depositar(valor){
         if(valor > 0){
