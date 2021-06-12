59f3c1c51d58aadb64979e75bae67bf943441243
Felipe Sousa <felipe_sousa@outlook.com>
2021-06-12 09:06:54 -0300

    Sistema interno

--- 4-javascript-orientacao-a-objetos/index.js
+++ 4-javascript-orientacao-a-objetos/index.js
@@ -1,5 +1,5 @@
 import{Cliente} from "./Cliente.js"
-import{ContaCorrente} from "./ContaCorrente.js"
+import{ContaCorrente} from "./Contas/ContaCorrente.js"
 
 const contaCorrenteRicardo = new ContaCorrente(new Cliente("Ricardo", 8752897528), 1005);
 console.log(`Criando a conta do ${contaCorrenteRicardo.cliente.nome}...`);
