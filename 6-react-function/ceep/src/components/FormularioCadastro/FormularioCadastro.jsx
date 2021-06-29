3856a63b0f0c2cc28e2b363f4e92f70fd366b830
Felipe Sousa <felipe_sousa@outlook.com>
2021-06-29 17:18:19 -0300

    Tratamento de erro

--- 6-react-function/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ 6-react-function/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,21 +1,87 @@
-import React from "react";
-import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
+import React, { useState } from "react";
+import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
-function FormularioCadastro() {
+function FormularioCadastro({aoEnviar, validarCPF}) {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [promocoes, setPromocoes] = useState(true);
+  const [novidades, setNovidades] = useState(false);
+  const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
   return (
-    <form>
-      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
-      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth/>
-      <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth/>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({nome, sobrenome, cpf, novidades, promocoes});
+      }}
+    >
+      <TextField
+        value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={sobrenome}
+        onChange={(event) => {
+          setSobrenome(event.target.value);
+        }}
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        value={cpf}
+        onChange={(event) => {
+          setCpf(event.target.value);
+        }}
 
-      <FormControlLabel 
-        control={<Switch name="promocoes" defaultChecked color="primary" />} 
-        label="Promoções"
+        onBlur={(event)=>{
+          const ehValido = validarCPF(cpf);
+          setErros({cpf:ehValido})
+        }}
+        error={!erros.cpf.valido}
+        helperText={erros.cpf.texto}
+        id="CPF"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
       />
 
-      <FormControlLabel 
-        control={<Switch name="novidades" defaultChecked color="primary" />} 
+      <FormControlLabel
+        label="Promoções"
+        control={
+          <Switch
+            checked={promocoes}
+            onChange={(event) => {
+              setPromocoes(event.target.checked);
+            }}
+            name="promocoes"
+            color="primary"
+          />
+        }
+      />
+
+      <FormControlLabel
         label="Novidades"
+        control={
+          <Switch
+            checked={novidades}
+            onChange={(event) => {
+              setNovidades(event.target.checked);
+            }}
+            name="novidades"
+            color="primary"
+          />
+        }
       />
 
       <Button type="submit" variant="contained" color="primary">
